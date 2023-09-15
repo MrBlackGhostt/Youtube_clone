5e80cb50cba194045e7870c21c394467c90fc38b
MrBlackGhostt <hemantkumar4213@icloud.com>
2023-09-15 12:34:31 +0530

    change icon and video alignment fix

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -10,7 +10,12 @@ const Videos = ({ videos, direction }) => {
   }
 
   return (
-    <Stack direction={direction} flexWrap='wrap' justifyContent='start' gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap='wrap'
+      justifyContent='start'
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
